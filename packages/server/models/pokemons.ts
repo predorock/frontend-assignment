3bcd835173719bb54c12d86e7ee420b591162805
Marco Predari <marco.predari@horsa.it>
2019-11-12 19:46:45 +0100

    setup environment. basic react client setup. added filterByType query on server

--- packages/server/models/pokemons.ts
+++ packages/server/models/pokemons.ts
@@ -10,6 +10,7 @@ interface Pokemon {
   id: string;
   name: string;
   types: string[];
+  classification: string;
 }
 
 const SIZE = 10;
@@ -32,16 +33,52 @@ export function query(args: {
     after === undefined
       ? identity
       : as =>
-          pipe(
-            as,
-            A.findIndex(a => a.id === after),
-            O.map(a => a + 1),
-            O.fold(() => as, idx => as.slice(idx))
-          );
+        pipe(
+          as,
+          A.findIndex(a => a.id === after),
+          O.map(a => a + 1),
+          O.fold(() => as, idx => as.slice(idx))
+        );
 
   const results: Pokemon[] = pipe(
     data,
     filterByQ,
+    sliceByAfter,
+    // slicing limit + 1 because the `toConnection` function should known the connection size to determine if there are more results
+    slice(0, limit + 1)
+  );
+  return toConnection(results, limit);
+}
+
+
+export function byType(args: {
+  after?: string;
+  limit?: number;
+  type: string;
+}): Connection<Pokemon> {
+  const { after, type, limit = SIZE } = args;
+
+  const filterByType: (as: Pokemon[]) => Pokemon[] =
+    // filter only if q is defined
+    type === undefined
+      ? identity
+      : A.filter(p => p.types.map(t => t.toLowerCase()).indexOf(type.toLowerCase()) >= 0);
+
+  const sliceByAfter: (as: Pokemon[]) => Pokemon[] =
+    // filter only if q is defined
+    after === undefined
+      ? identity
+      : as =>
+        pipe(
+          as,
+          A.findIndex(a => a.id === after),
+          O.map(a => a + 1),
+          O.fold(() => as, idx => as.slice(idx))
+        );
+
+  const results: Pokemon[] = pipe(
+    data,
+    filterByType,
     sliceByAfter,
     // slicing limit + 1 because the `toConnection` function should known the connection size to determine if there are more results
     slice(0, limit + 1)
